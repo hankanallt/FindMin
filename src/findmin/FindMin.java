0d13aadba4093ed727068599cc3914f15cb8af5e
Harkanwal Singh <hankanallt@gmail.com>
2019-12-05 18:21:09 +0100

    First Commit

---
+++ src/findmin/FindMin.java
@@ -0,0 +1,41 @@
+
+package findmin;
+
+
+public class FindMin {
+
+    
+    public static void main(String[] args) {
+        
+        
+       
+findMin(3, 56, 6, 99, 67);
+}
+
+    private static void findMin(int a, int b, int c, int d, int e) {
+
+    
+int min = a;
+if (b < min) {
+min=b;
+}
+if (c < min) {
+min = c;
+}
+if (d < min) {
+min = d;
+}
+if (e < min) {
+min = e;
+}
+
+
+System.out.println(min);
+}
+
+    }
+
+
+    
+    
+
